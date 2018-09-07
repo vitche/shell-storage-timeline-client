b7b808834145bd71feecfb073bbe9842b9efabd2
Vitche Research Team Developer <developer@vitche.com>
2018-09-08 00:07:50 +0300

    1. Adding numbers. 2. Listing all strings / numbers.

--- storage-timeline.sh
+++ storage-timeline.sh
@@ -3,16 +3,19 @@
 case $2 in
 addString)
     echo "Executing TimeLine.addString"
-    wget "$1/timeline/add/string" --no-check-certificate --post-data "schema=$3&timeLine=$4&value=$5"
+    wget "$1/timeline/add/string" --no-check-certificate --post-data "schema=$3&timeLine=$4&value=$5" -O-
     ;;
 addNumber)
     echo "Executing TimeLine.addNumber"
+    wget "$1/timeline/add/number" --no-check-certificate --post-data "schema=$3&timeLine=$4&value=$5" -O-
     ;;
 allStrings)
     echo "Executing TimeLine.allStrings"
+    wget "$1/timeline/all/strings?schema=$3&timeLine=$4" --no-check-certificate -O-
     ;;
 allNumbers)
     echo "Executing TimeLine.allNumbers"
+    wget "$1/timeline/all/numbers?schema=$3&timeLine=$4" --no-check-certificate -O-
     ;;
 schemaCreate)
     echo "Executing Schema.create"
--- storage-timeline.test.sh
+++ storage-timeline.test.sh
@@ -1,2 +1,5 @@
 dos2unix *.sh
 ./storage-timeline.sh https://34.239.11.167:82 addString test test test
+./storage-timeline.sh https://34.239.11.167:82 addNumber test test 1
+./storage-timeline.sh https://34.239.11.167:82 allStrings test test
+./storage-timeline.sh https://34.239.11.167:82 allNumbers test test
