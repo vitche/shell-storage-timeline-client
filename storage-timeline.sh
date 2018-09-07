67fdc5cc9a6ef7882f49b8f34657aece4fe060af
Vitche Research Team Developer <developer@vitche.com>
2018-09-07 23:30:50 +0300

    1. Converted line breaks to *NIX format.

---
+++ storage-timeline.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+case $0 in
+addString)
+    echo "addString"
+    ;;
+addNumber)
+    echo "addNumber"
+    ;;
+allStrings)
+    echo "allStrings"
+    ;;
+allNumbers)
+    echo "allNumbers"
+    ;;
+timeLineCreate)
+    ;;
+esac
