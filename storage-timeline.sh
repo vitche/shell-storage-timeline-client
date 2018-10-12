dd4ded431cf79bb782d2d927b8996bdc95ff4988
Vitche Research Team Developer <developer@vitche.com>
2018-10-12 19:07:20 +0300

    ))

--- storage-timeline.sh
+++ storage-timeline.sh
@@ -1,35 +1,38 @@
 #!/usr/bin/env bash
-
 case $1 in
 install)
-    rm /usr/bin/storage-timeline.sh
+    if [ -f /usr/bin/storage-timeline.sh ]; then
+        rm /usr/bin/storage-timeline.sh
+    fi
     wget https://raw.githubusercontent.com/vitche/shell-storage-timeline-client/master/storage-timeline.sh -O- > /usr/bin/storage-timeline.sh
     chmod +x /usr/bin/storage-timeline.sh
     exit 0
-esac
-
-case $2 in
-addString)
-    echo "Executing TimeLine.addString"
-    wget "$1/timeline/add/string" --no-check-certificate --post-data "schema=$3&timeLine=$4&value=$5" -O-
-    ;;
-addNumber)
-    echo "Executing TimeLine.addNumber"
-    wget "$1/timeline/add/number" --no-check-certificate --post-data "schema=$3&timeLine=$4&value=$5" -O-
-    ;;
-allStrings)
-    echo "Executing TimeLine.allStrings"
-    wget "$1/timeline/all/strings?schema=$3&timeLine=$4" --no-check-certificate -O-
-    ;;
-allNumbers)
-    echo "Executing TimeLine.allNumbers"
-    wget "$1/timeline/all/numbers?schema=$3&timeLine=$4" --no-check-certificate -O-
-    ;;
-schemaCreate)
-    echo "Executing Schema.create"
-    wget "$1/schema/create" --no-check-certificate --post-data "schema=$3&timeLine=$4" -O-
     ;;
 *)
-    echo "Unable to execute unknown command $2"
+    case $2 in
+    addString)
+        echo "Executing TimeLine.addString"
+        wget "$1/timeline/add/string" --no-check-certificate --post-data "schema=$3&timeLine=$4&value=$5" -O-
+        ;;
+    addNumber)
+        echo "Executing TimeLine.addNumber"
+        wget "$1/timeline/add/number" --no-check-certificate --post-data "schema=$3&timeLine=$4&value=$5" -O-
+        ;;
+    allStrings)
+        echo "Executing TimeLine.allStrings"
+        wget "$1/timeline/all/strings?schema=$3&timeLine=$4" --no-check-certificate -O-
+        ;;
+    allNumbers)
+        echo "Executing TimeLine.allNumbers"
+        wget "$1/timeline/all/numbers?schema=$3&timeLine=$4" --no-check-certificate -O-
+        ;;
+    schemaCreate)
+        echo "Executing Schema.create"
+        wget "$1/schema/create" --no-check-certificate --post-data "schema=$3&timeLine=$4" -O-
+        ;;
+    *)
+        echo "Unable to execute unknown command $2"
+        ;;
+    esac
     ;;
 esac
